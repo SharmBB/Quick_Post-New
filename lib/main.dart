f931ac3d5441235cefd59b1614a1a2125d6e0bbc
Shan Jathu <shanjathu@Shans-MacBook-Pro.local>
2021-08-14 21:52:28 +0530

    vs code changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
+ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:post/home/home.dart';
 
 import 'package:post/sign_in/signin.dart';
 
@@ -14,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
